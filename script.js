11bb6c147b21198bdfaddbda58f25c24a39f6724
Luciano Lanes Lopes <llaneslopes@gmail.com>
2021-05-07 18:29:52 -0300

    Requisito 7 concluido

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
 
-
-
     let textoDigitado = document.querySelector('#texto-tarefa');
     
     let botaoAdicionar = document.querySelector('#criar-tarefa');
-
+    
     botaoAdicionar.addEventListener('click', function (){
 
         if (textoDigitado.value.length > 0){
        let listaOrdenada = document.querySelector('#lista-tarefas');
 
        let criarLi = document.createElement('li');
-        
-       criarLi.innerText = textoDigitado.value;
-
+        criarLi.className = 'itemDaLista';
+        criarLi.innerText = textoDigitado.value; 
+        criarLi.addEventListener('click', mudarBackground);
         listaOrdenada.appendChild(criarLi);
 
         textoDigitado.value = '';
         }
     });
+
+
+    function mudarBackground (event) {
+    event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+    }
