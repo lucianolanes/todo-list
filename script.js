64ea2352f057cdbee16d5144b551ec502f143aec
Luciano Lanes Lopes <llaneslopes@gmail.com>
2021-05-08 12:21:55 -0300

    Requisito 9 concluido

--- script.js
+++ script.js
@@ -1,30 +1,39 @@
 
-    let textoDigitado = document.querySelector('#texto-tarefa');
+let textoDigitado = document.querySelector('#texto-tarefa');
     
-    let botaoAdicionar = document.querySelector('#criar-tarefa');
+let botaoAdicionar = document.querySelector('#criar-tarefa');
     
-    botaoAdicionar.addEventListener('click', function (){
+botaoAdicionar.addEventListener('click', function (){
 
-        if (textoDigitado.value.length > 0){
-       let listaOrdenada = document.querySelector('#lista-tarefas');
+    if (textoDigitado.value.length > 0){
+    let listaOrdenada = document.querySelector('#lista-tarefas');
 
-       let criarLi = document.createElement('li');
-        criarLi.className = 'itemDaLista';
-        criarLi.innerText = textoDigitado.value; 
-        criarLi.addEventListener('click', mudarBackground);
-        listaOrdenada.appendChild(criarLi);
-        textoDigitado.value = '';
-        }
-    });
-
-
-    function mudarBackground (event) {
-        let itensDaLista = document.getElementsByClassName('itemDaLista');
-        for (index = 0; index < itensDaLista.length; index +=1){
-            if (itensDaLista[index] === event.target){
-                event.target.style.backgroundColor = 'rgb(128, 128, 128)';
-            } else {
-                itensDaLista[index].style.backgroundColor = 'white';
-            }
-        }    
+    let criarLi = document.createElement('li');
+    criarLi.className = 'itemDaLista';
+    criarLi.innerText = textoDigitado.value; 
+    criarLi.addEventListener('click', mudarBackground);
+    criarLi.addEventListener('dblclick', riscar);
+    listaOrdenada.appendChild(criarLi);
+    textoDigitado.value = '';
     }
+});
+
+
+function mudarBackground (event) {
+    let itensDaLista = document.getElementsByClassName('itemDaLista');
+    for (index = 0; index < itensDaLista.length; index +=1){
+        if (itensDaLista[index] === event.target){
+        event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+        } else {
+        itensDaLista[index].style.backgroundColor = 'white';
+        }
+    }    
+}
+
+function riscar (event) {
+    if (event.target.classList.contains('completed')){
+    event.target.classList.remove('completed');
+    } else {
+    event.target.classList.add('completed');
+  }
+}
